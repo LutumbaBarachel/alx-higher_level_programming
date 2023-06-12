356b77cfc573d658aaddba44ef9849272d391ecf
LutumbaBarachel <barachellutumbanzengo@gmail.com>
2023-06-12 08:57:39 -0700

    multiple returns

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,12 +1,5 @@
 #!/usr/bin/python3
-# 9-max_integer.py
-
-def max_integer(my_list=[]):
-    """Find the biggest integer of a list."""
-    if len(my_list) == 0:
-        return (None)
-    big = my_list[0]
-    for i in range(len(my_list)):
-        if my_list[i] > big:
-            big = my_list[i]
-    return (big)
+def multiple_returns(sentence):
+    length = len(sentence)
+    first_char = sentence[0] if length > 0 else None
+    return (length, first_char)
