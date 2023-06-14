c1505b926f8929c60e2489661bc1eadf8afb58c7
LutumbaBarachel <barachellutumbanzengo@gmail.com>
2023-06-14 10:15:54 -0700

    calculating

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
+import calculator_1 as calc
+
 a = 10
 b = 5
 
-import calculator_1
-
-add_result = calculator_1.add(a, b)
-sub_result = calculator_1.subtract(a, b)
-mul_result = calculator_1.multiply(a, b)
-div_result = calculator_1.divide(a, b)
+add_result = calc.add(a, b)
+sub_result = calc.subtract(a, b)
+mul_result = calc.multiply(a, b)
+div_result = calc.divide(a, b)
 
 print("Addition: {}".format(add_result))
 print("Subtraction: {}".format(sub_result))
